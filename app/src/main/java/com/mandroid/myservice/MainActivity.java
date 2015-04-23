2007cd92940d534dae992757b016f155532d8ff6
wilk3ns <k.qurbanli@gmail.com>
2015-04-23 15:45:34 +0500

    Added emergency call support

--- app/src/main/java/com/mandroid/myservice/MainActivity.java
+++ app/src/main/java/com/mandroid/myservice/MainActivity.java
@@ -2,9 +2,8 @@ package com.mandroid.myservice;
 
 
 import android.app.AlertDialog;
-import android.app.Application;
-import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
@@ -18,7 +17,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
